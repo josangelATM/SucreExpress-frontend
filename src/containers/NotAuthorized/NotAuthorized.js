a04201c569bf0ced4653c40010a12b0a4e387f78
JoséAngel Sánchez <joseangel19.lol@gmail.com>
2021-05-10 18:40:22 -0500

    Sorry, changes in main

--- src/containers/NotAuthorized/NotAuthorized.js
+++ src/containers/NotAuthorized/NotAuthorized.js
@@ -3,8 +3,8 @@ import { faBug } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 const NotAuthorized = () =>(
     <div>
-        <FontAwesomeIcon/>
-        Lo siento, no estás autorizado para ver está página
+        <FontAwesomeIcon icon={faBug} size='10x'/>
+        Lo siento, no estás autorizado para ver está página F
     </div>
 )
 
